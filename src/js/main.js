e22406592ec2b41c36393a8331a2c9b291dbbc26
GabSiccion <gabsiccion@outlook.com>
2022-12-01 23:24:41 +0800

    importing functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,8 @@
 import * as bootstrap from "bootstrap";
-import { Header, Home } from "./components";
+import { Header } from "./components";
 import "./navselect";
 
 //Adds the navigation bar
 customElements.define("main-header", Header);
-
-//Adds the home content
-customElements.define("home-content", Home);
 
 //populate course dropdown
