2e8a2d47190d5460dfe3273dce7d77940338df4a
GabSiccion <gabsiccion@outlook.com>
2022-12-06 17:40:24 +0800

    Topic UI cards adjusted

--- src/js/exploration.js
+++ src/js/exploration.js
@@ -39,9 +39,67 @@ onValue(coursesRef, (snapshot) => {
 
 	//page content
 	let contentbox = $(".content-box");
-	let test = document.createElement("h1");
-	test.innerHTML = "TEST";
-	contentbox.append(test);
+
+	let container = document.createElement("div");
+	container.className = "container mt-4";
+
+	let h1 = document.createElement("h1");
+	h1.innerHTML = data[selectedcourse]["header-text"];
+
+	let p = document.createElement("p");
+	p.innerHTML = data[selectedcourse]["article-text"];
+
+	container.append(h1, p);
+	var trackcontainer = document.createElement("div");
+	trackcontainer.id = "trackcontainer";
+
+	for (obj in data[selectedcourse]["tracks"]) {
+		let track = document.createElement("div");
+		track.className = "mt-4 mb-4";
+
+		let trackname = document.createElement("h2");
+		trackname.innerHTML = obj;
+
+		let tracktext = document.createElement("p");
+		tracktext.innerHTML = data[selectedcourse]["tracks"][obj]["track-text"];
+
+		track.append(trackname, tracktext);
+
+		let cardcontainer = document.createElement("div");
+		cardcontainer.className = "card-container";
+
+		for (obj2 in data[selectedcourse]["tracks"][obj]["track-topics"]) {
+			var card = document.createElement("div");
+			card.className = "card m-1";
+			card.style = "width: 32rem";
+
+			let cardbody = document.createElement("div");
+			cardbody.className = "card-body";
+
+			let cardtitle = document.createElement("h4");
+			cardtitle.className = "card-title";
+			cardtitle.innerHTML = obj2;
+
+			let cardtext = document.createElement("p");
+			cardtext.className = "card-text";
+			cardtext.innerHTML =
+				data[selectedcourse]["tracks"][obj]["track-topics"][obj2]["topic-text"];
+
+			let cardlink = document.createElement("button");
+			cardlink.className = "btn btn-primary";
+			cardlink.innerHTML = "View topics and lessons";
+			let src =
+				data[selectedcourse]["tracks"][obj]["track-topics"][obj2]["topic-text"];
+
+			cardbody.append(cardtitle, cardtext, cardlink);
+			card.append(cardbody);
+			cardcontainer.append(card);
+		}
+		track.append(cardcontainer);
+		trackcontainer.append(track);
+	}
+	container.append(trackcontainer);
+	contentbox.append(container);
 
 	/*
 	//page content
